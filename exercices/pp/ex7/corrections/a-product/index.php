3f32b21d2c4a41dfac83b699a3fdc3e14b6fe223
Raphaël Lefebvre <raphael@raphaellefebvre.be>
2023-03-29 15:55:39 +0200

    update

---
+++ exercices/pp/ex7/corrections/a-product/index.php
@@ -0,0 +1,14 @@
+<?php
+
+$article = [
+    'isPremium' => true,
+    'title' => 'Pantalon ample en lin',
+    'price' => '69,99 €',
+    'color' => ['title' => 'Noir', 'value' => '#272628'],
+    'images' => [
+        './data/images/1-0.jpg',
+        './data/images/1-1.jpg'
+    ],
+];
+
+var_dump($article);
